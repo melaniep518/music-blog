27cb42b59cb19bc98ef27f43d64508eb77981e45
Melanie Williams <melaniep518@gmail.com>
2017-01-08 21:02:58 -0500

    more artist routes

--- server/routes/artistRoute.js
+++ server/routes/artistRoute.js
@@ -17,16 +17,49 @@ function getAllArtists(req, res) {
 		})
 }
 
+// ********** GET artist by name **********
+function getArtistByName(req, res) {
+	Artist.findOne({
+		where: {
+			name: req.params.name
+		}
+	})
+	.then(function(artist) {
+		res.send(artist);
+	})
+}
+
+
 // ********** DELETE artist by id **********
-function deleteArtist(req, res) {
+function deleteArtistById(req, res) {
 	Artist.destroy({
 		where: {
 			id: req.params.id
 		}
 	})
 	.then(function(artist) {
-		console.log('Artist has been deleted');
-		res.send('Artist has been deleted')
+		res.send('Artist has been deleted.')
+	})
+}
+
+// ********** DELETE artist by name **********
+// SHOULD PROBABLY BE SEPERATED INTO GET AND DELETE REQUESTS???
+function deleteArtistByName(req, res) {
+	Artist.findOne({
+		where: {
+			name: req.params.name
+		}
+	})
+	.then(function(artist) {
+		console.log('ARTIST', artist.dataValues.id)
+		Artist.destroy({
+			where: {
+				id: artist.dataValues.id
+			}
+		})
+	})
+	.then(function(artist) {
+		res.send('Artist has been deleted.')
 	})
 }
 
@@ -39,14 +72,36 @@ function postNewArtist(req, res) {
 	})
 	.then(function(artist) {
 		console.log('REQ BODY:', artist);
-		res.send('You have added a new artist')
+		res.send(artist)
+	})
+}
+
+// ********** PUT artist name **********
+function putArtistName(req, res) {
+	Artist.update({
+		name: req.body.artistName
+	},
+	{
+		where: {
+			id: req.params.id
+		}
+	})
+	.then(function(artist) {
+		console.log(artist)
+		res.send('Artist has been updated.')
 	})
 }
 
 // ********** ROUTES **********
-router.route('/:id')
+router.route('/name/:name')
+	.delete(deleteArtistByName)
+	.get(getArtistByName)
+	
+
+router.route('/id/:id')
 	.get(getArtistById)
-	.delete(deleteArtist)
+	.delete(deleteArtistById)
+	.put(putArtistName)
 
 router.route('/')
 	.get(getAllArtists)
