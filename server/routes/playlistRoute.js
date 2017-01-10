a92d359a64834c15082d16f87ced777545c04323
Melanie Williams <melaniep518@gmail.com>
2017-01-09 19:13:28 -0500

    playlist routes

--- server/routes/playlistRoute.js
+++ server/routes/playlistRoute.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
 const Playlist = require('../models/index').Playlist;
 
-// ********** POST new playlist **********
+// ********** DELETE requests **********
+function deletePlaylistByTitle(req, res) {
+  Playlist.findOne({
+    where: {
+      title: req.params.title
+    }
+  })
+  .then(function(playlist) {
+    Playlist.destroy({
+      where: {
+        id: playlist.dataValues.id
+      }
+    })
+  })
+  .then(function(playlist) {
+    res.send('Playlist has been deleted.')
+  })
+}
+
+// ********** GET requests **********
+function getAllPlaylists(req, res) {
+  Playlist.findAll()
+  .then(function(playlist) {
+    console.log(playlist);
+    res.send(playlist);
+  })
+}
+
+// ********** POST requests **********
 function postNewPlaylist(req, res) {
   Playlist.findOrCreate({
     where: {
@@ -18,6 +46,19 @@ function postNewPlaylist(req, res) {
 // ********** ROUTES **********
 router.route('/')
   .post(postNewPlaylist)
+  .get(getAllPlaylists)
+
+router.route('/title/:title')
+  .delete(deletePlaylistByTitle)
+//   .get(getPlaylistByTitle)
+//   .put(putPlaylistTitleByTitle)
+  
+
+// router.route('/id/:id')
+//   .get(getPlaylistById)
+//   .delete(deletePlaylistById)
+//   .put(putPlaylistNameById)
+
 
 
 module.exports = router;
